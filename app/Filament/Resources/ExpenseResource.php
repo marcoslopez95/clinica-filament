a96ab0998726194e58adeb1fc268137be915b54a
edison-devs <edisonhernantriana09@gmail.com>
2025-12-18 18:49:14 -0400

    expense form y table

--- app/Filament/Resources/ExpenseResource.php
+++ app/Filament/Resources/ExpenseResource.php
@@ -3,27 +3,68 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ExpenseResource\Pages;
-use App\Filament\Resources\ExpenseResource\RelationManagers;
 use App\Models\Expense;
-use Filament\Forms;
+use App\Models\Currency;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ForceDeleteAction;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-
 class ExpenseResource extends Resource
 {
     protected static ?string $model = Expense::class;
 
+    protected static ?string $slug = 'expenses';
+    
+    protected static ?string $navigationGroup = 'Configuración';
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $modelLabel = 'Gastos';
+    protected static ?string $pluralModelLabel = 'Gastos';
+    protected static ?string $navigationLabel = 'Gastos';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Textarea::make('description')
+                    ->label('Descripción')
+                    ->required()
+                    ->rows(1),
+
+                TextInput::make('price')
+                    ->label('Precio')
+                    ->required()
+                    ->numeric(),
+
+                Select::make('currency_id')
+                ->label('Moneda')
+                ->relationship('currency', 'name')
+                ->required()
+                ->reactive()
+                ->preload()
+                ->afterStateUpdated(function ($state, callable $set) {
+                    $set('exchange', Currency::find($state)->exchange);
+                }),
+
+                Select::make('expense_category_id')
+                    ->label('Categoría')
+                    ->relationship('category', 'name')
+                    ->required()
+                    ->preload(),
+
+                TextInput::make('exchange')
+                    ->label('Tasa de Cambio')
+                    ->readOnly()
+                    ->numeric()
+                    ->required()
             ]);
     }
 
@@ -31,13 +72,20 @@ class ExpenseResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('description')->label('Descripción')->limit(50),
+                TextColumn::make('price')->label('Precio')->sortable(),
+                TextColumn::make('currency.name')->label('Moneda')->sortable(),
+                TextColumn::make('category.name')->label('Categoría')->sortable(),
+                TextColumn::make('exchange')->label('Tasa de Cambio')->sortable(),
             ])
             ->filters([
-                //
+                TrashedFilter::make(),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                EditAction::make(),
+                DeleteAction::make(),
+                RestoreAction::make(),
+                ForceDeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
