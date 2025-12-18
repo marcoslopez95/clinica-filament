95c70a6426bdb0aaf10c0e5e45fd165a01403ce9
edison-devs <edisonhernantriana09@gmail.com>
2025-12-18 16:14:14 -0400

    Creacion del modelo Expense y relaciones con ExpenseCategory

--- app/Models/ExpenseCategory.php
+++ app/Models/ExpenseCategory.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ExpenseCategory extends Model
 {
     use SoftDeletes;
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
 }
