cf53e597b83470c8c1af6562f746a5e5c5de376a
edison-devs <edisonhernantriana09@gmail.com>
2026-01-02 18:43:25 -0400

    modelo de devoluciones

--- app/Models/Refund.php
+++ app/Models/Refund.php
@@ -6,50 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Refund extends Model
+class Refun extends Model
 {
     use SoftDeletes;
 
     protected $fillable = [
-        'amount',
         'payment_id',
-        'payment_method_id',
         'currency_id',
+        'payment_method_id',
+        'amount',
         'exchange',
     ];
-
-    protected static function booted(): void
-    {
-        static::created(function (Refund $refund) {
-            $refund->payment->invoice->updateStatusIfPaid();
-        });
-
-        static::updated(function (Refund $refund) {
-            $refund->payment->invoice->updateStatusIfPaid();
-        });
-
-        static::deleted(function (Refund $refund) {
-            $refund->payment->invoice->updateStatusIfPaid();
-        });
-    }
-
-    public function invoice()
-    {
-        return $this->payment->invoice();
-    }
 
     public function payment(): BelongsTo
     {
         return $this->belongsTo(Payment::class);
     }
 
-    public function paymentMethod(): BelongsTo
-    {
-        return $this->belongsTo(PaymentMethod::class);
-    }
-
     public function currency(): BelongsTo
     {
         return $this->belongsTo(Currency::class);
+    }
+
+    public function paymentMethod(): BelongsTo
+    {
+        return $this->belongsTo(PaymentMethod::class);
     }
 }
---
+++ database/migrations/2026_01_02_213713_create_refuns_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('refuns', function (Blueprint $table) {
+            $table->id();
+$table->foreignId('payment_id');
+$table->foreignId('currency_id');
+$table->foreignId('payment_method_id');
+$table->float('amount');
+$table->float('exchange');
+$table->timestamps();
+$table->softDeletes();//
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('refuns');
+    }
+};
