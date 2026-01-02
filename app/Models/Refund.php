f964018122e8d924809e7a0cfe3ad41fb337d9ca
edison-devs <edisonhernantriana09@gmail.com>
2026-01-02 18:43:54 -0400

    arreglo de migracion y modelo

--- app/Models/Refund.php
+++ app/Models/Refund.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Refun extends Model
+class Refund extends Model
 {
     use SoftDeletes;
 
--- database/migrations/2026_01_02_213713_create_refuns_table.php
+++ database/migrations/2026_01_02_213713_create_refuns_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
-        Schema::create('refuns', function (Blueprint $table) {
+        Schema::create('refunds', function (Blueprint $table) {
             $table->id();
-$table->foreignId('payment_id');
-$table->foreignId('currency_id');
-$table->foreignId('payment_method_id');
-$table->float('amount');
-$table->float('exchange');
-$table->timestamps();
-$table->softDeletes();//
+            $table->foreignId('payment_id');
+            $table->foreignId('currency_id');
+            $table->foreignId('payment_method_id');
+            $table->float('amount');
+            $table->float('exchange');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('refuns');
+        Schema::dropIfExists('refunds');
     }
 };
